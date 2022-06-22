3e1ec712b362e99aee78e8086477598faca2240d
Lucas-Marques-Leao <lucasmarquesleao@outlook.com>
2022-06-22 03:32:36 -0300

    feat: apollo-client initial settings

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,12 +1,17 @@
+import { ApolloProvider } from "@apollo/client";
+import client from "./services/api";
+
 import Routes from "./router";
 import "./styles/global.css";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Routes />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Routes />
+      </div>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
