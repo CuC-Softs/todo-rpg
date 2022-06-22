78c26f9b04dfe793c2e71f155c09c8a7dd020005
Manuel-Antunes <nerdbroz2@gmail.com>
2022-06-22 00:34:20 -0300

    chore: add routers of app and auth in routes file

--- packages/frontend/src/router/index.tsx
+++ packages/frontend/src/router/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./AppRoutes";
+import AuthRoutes from "./AuthRoutes";
 
 const Routes: React.FC = () => {
   const isAuthenticated = false;
   return (
     <BrowserRouter>
+      {isAuthenticated ? <AppRoutes /> : <AuthRoutes />}
     </BrowserRouter>
   );
 };
