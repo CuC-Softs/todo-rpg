a606c5c89941c77cd9fe9d10184289bd38825847
Manuel-Antunes <nerdbroz2@gmail.com>
2022-06-22 00:31:15 -0300

    feat: implement react router dom base structure

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,6 +1,10 @@
+import Routes from "./router";
+import "./styles/global.css";
+
 function App() {
   return (
     <div className="App">
+      <Routes />
     </div>
   );
 }
---
+++ packages/frontend/src/router/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+const Routes: React.FC = () => {
+  const isAuthenticated = false;
+  return (
+    <BrowserRouter>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
